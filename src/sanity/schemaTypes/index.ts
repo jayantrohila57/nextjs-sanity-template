666842137e0225908d8791e7e9cef32a77f9aae1
jrohila57 <58130857+jayantrohila57@users.noreply.github.com>
2026-01-12 23:37:59 +0530

    Rename files from camelCase to kebab-case
    - Rename schemaType files: authorType.ts  author-type.ts, etc.
    - Rename other source files to use kebab-case naming convention
    - Update import statements in schemaTypes/index.ts
    - All files now follow consistent kebab-case naming convention
    - Lint passes successfully

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,15 +1,15 @@
 import type { SchemaTypeDefinition } from "sanity";
 
-import { authorType } from "./authorType";
-import { blockContentType } from "./blockContentType";
-import { categoryType } from "./categoryType";
-import { legalType } from "./legalType";
-import { navigationType } from "./navigationType";
-import { pageType } from "./pageType";
-import { postType } from "./postType";
-import { redirectType } from "./redirectType";
-import { seoType } from "./seoType";
-import { siteSettingsType } from "./siteSettingsType";
+import { authorType } from "./author-type";
+import { blockContentType } from "./block-content-type";
+import { categoryType } from "./category-type";
+import { legalType } from "./legal-type";
+import { navigationType } from "./navigation-type";
+import { pageType } from "./page-type";
+import { postType } from "./post-type";
+import { redirectType } from "./redirect-type";
+import { seoType } from "./seo-type";
+import { siteSettingsType } from "./site-settings-type";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
